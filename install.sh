d73a5c6d83bd57ce281cb4c58b6a32ebb7066be2
root <fivestarg777@yahoo.com>
2020-03-18 17:39:17 -0400

    Rewrite installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,54 @@
-#/bin/bash
+#!/bin/bash
+########Skywire XNS#########
 
-#check OS architecture
+echo -e "\e[1;97;44m       SSS  KK   K YY  YY  CCC   OOO  IIIIII NN  NN                 \e[0m"
+echo -e "\e[1;97;44m      SS SS KK  K  YY  YY CC  C OO OO   II   NN  NN                 \e[0m"
+echo -e "\e[1;97;44m      SSS   KK K    YYYY  CC    OO OO   II   NN  NN                 \e[0m"
+echo -e "\e[1;97;44m        SS  KKK      YY   CC    OO OO   II   NNN NN                 \e[0m"
+echo -e "\e[1;97;44m         SS KK K     YY   CC    OO OO   II   NN NNN                 \e[0m"
+echo -e "\e[1;97;44m      SS SS KK  K    YY   CC  C OO OO   II   NN  NN                 \e[0m"
+echo -e "\e[1;97;44m       SSS  KK   K   YY    CCC   OOO  IIIIII NN  NN                 \e[0m"
 
-#while loop
+echo -e "\e[96;5mWelcome to the Skywire XNS Installer\e[25m"
 
-#check required dependancies
+echo -e "\e[31mInstalling dependancies\e[0m"
 
-#installation options
+apt-get update  # To get the latest package lists
+apt-get install curl -y
+apt-get install wget -y
+apt-get install python -y
+apt-get install python-zmq -y
+apt-get install python-twisted-core -y
+apt-get install ufw -y
+apt-get install screen -y
+apt-get install openssl -y
+apt-get install eog -y
+#apt-get install <package name> -y
+#apt-get install <package name> -y
 
-#install missing dependancies
+#Install SSL Cert (User Prompt, use defaults)
+echo -e "\e[38;5;166mCreating SSL certificate\e[0m"
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes 
+
+#Configure Twisted
+# Installs Twisted if needed, then starts a xns-server in a twisted instance,
+# listening on port 8001 and 8002.
+echo -e "\e[38;5;11mOpening ports for Skywire XNS\e[0m"
+
+ufw enable
+ufw allow 8001
+ufw allow 8002
+ufw reload
+
+
+echo -e "\e[5;96mStarting Skywire XNS\e[0m"
+sleep 3
+kill -9 `ps -ef | grep 'Skywire-XNS'  | grep -v grep | awk '{print $2}'`
+kill -9 `ps -ef | grep 'xns-server.py'  | grep -v grep | awk '{print $2}'`
+#kill -9 $(ps -elf | grep Skywire-XNS| awk {'print $4'})
+#kill -9 $(ps -elf | grep xns-server.py| awk {'print $4'})
+twistd -y xns-server.py
+screen -dmS Skywire-XNS ./skywire-xns.py
+
+echo -e "\e[38;5;10mSkywire XNS is now installed and running\nUse 'screen -ls' to find PID\nthen use 'screen -r PID'\nto enter Skywire XNS CLI console\e[0m"
+
