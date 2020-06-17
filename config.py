0074d7fef9cefe84e3a1208cf784e6ce8064299d
devzone777 <fivestar777@yahoo.com>
2020-06-17 15:26:06 -0400

    Change ports, clarify comments 	modified:   config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 import socket
 
-default_port = 8001 # default port for adding a new server
-listen_port = 8001 # port for the server to listen on
+default_port = 8003 # default port when adding a new server
+listen_port = 8003 # server listens on this port
 my_address = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
               if not ip.startswith("10.")][0]
-dns_module_listen_port = 8002
-default_ttl = 60   # in seconds
-default_record_lifetime = 3600 * 24 * 30 # one month, duh
+dns_module_listen_port = 8004
+default_ttl = 60   # time to live in seconds
+default_record_lifetime = 3600 * 24 * 30 # 1 month
