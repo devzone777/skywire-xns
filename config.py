52392c1e339058c71357369cb0f3e35a51e671f4
devzone777 <fivestarg777@yahoo.com>
2020-06-18 09:37:26 -0400

    adjust config.py 	modified:   config.py 	modified:   startxns.sh

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import socket
 default_port = 8003 # default port when adding a new server
 listen_port = 8003 # server listens on this port
 my_address = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-              if not ip.startswith("127.")][0]
+              if not ip.startswith("1.")][0]
 dns_module_listen_port = 8004
 default_ttl = 60   # time to live in seconds
 default_record_lifetime = 3600 * 24 * 30 # 1 month
--- startxns.sh
+++ startxns.sh
@@ -2,5 +2,7 @@
 # Startup script for Skywire XNS P2P Server
 
 twistd -y xns-server.py
+
 echo -e "\033[32mSkywire XNS is now running\n\033[0m"
-screen -dmS skywire-xns ./skywire-xns.py
+
+screen -dmS Skywire-XNS ./skywire-xns.py
